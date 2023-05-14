42e1d223cd5be8ee19a243dc86121232db014ed5
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-05-14 18:21:52 -0300

    manipulando arrays

--- 08_manipulação_arrays.php
+++ 08_manipulação_arrays.php
@@ -1,3 +1,23 @@
 <?php 
 
+$infoCompany = [
+    'name' => 'EspecializaTi',
+    'founder' => 'Carlos Ferreira',
+    'year_current' => 2023,
+];
 
+$infoCompanyTwo = [
+    'courses' => ['php','js','laravel'],
+    'total_courses' => 26,
+];
+
+// var_dump(array_keys($infoCompany));
+
+// var_dump(array_values($infoCompany));
+
+// var_dump(count($infoCompany));
+
+$infoCompany = array_merge($infoCompany, $infoCompanyTwo);
+
+echo '<pre>';
+var_dump($infoCompany);
