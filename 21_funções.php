486383d0e91cd28e4490874066430cc51f0fdbf9
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-05-15 15:50:37 -0300

    funções

---
+++ 21_funções.php
@@ -0,0 +1,12 @@
+<?php
+
+function testFunctions():string
+{
+  return 'Sou uma função <br>';
+}
+
+echo testFunctions();
+echo testFunctions();
+echo testFunctions();
+echo testFunctions();
+echo testFunctions();
