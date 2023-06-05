e23f8fcaec8658860ae6e7d148fbcc9feb3a4a62
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-06-05 17:10:53 -0300

    Operadores de relacionais

---
+++ 37_operadores_relacionais.php
@@ -0,0 +1,40 @@
+<?php
+
+$a = 2;
+$b = '2';
+
+if ($a == $b) {
+  echo 'IF';
+} else {
+  echo 'ELSE';
+}
+
+if ($a > $b) {
+  echo 'IF';
+} else {
+  echo 'ELSE';
+}
+
+if ($a < $b) {
+  echo 'IF';
+} else {
+  echo 'ELSE';
+}
+
+if ($a >= $b) {
+  echo 'IF';
+} else {
+  echo 'ELSE';
+}
+
+if ($a <= $b) {
+  echo 'IF';
+} else {
+  echo 'ELSE';
+}
+
+if ($a != $b) {
+    echo 'IF';
+} else {
+    echo 'ELSE';
+}
