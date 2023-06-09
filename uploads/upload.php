1dd46a391f39ab162f40f81b2d378c9255c8d6aa
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-06-09 09:36:27 -0300

    Upload de arquivos no PHP

---
+++ uploads/upload.php
@@ -0,0 +1,11 @@
+<?php 
+$photo = $_FILES['foto'];
+
+$path = 'uploads/imgs/';
+
+if (move_uploaded_file($photo['tmp_name'], $path . $photo['name'])) {
+  echo 'Sucesso!';
+}else {
+  echo 'Error!';
+}
+
