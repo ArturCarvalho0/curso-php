a2d2ac25808b0da7b53fef8f745ebfe38e9bc659
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-06-09 07:05:40 -0300

    Pegar dados de formulários com PHP

--- forms/form.php
+++ forms/form.php
@@ -1,3 +1,16 @@
 <?php
 
-var_dump($_POST['nome']);
+echo "Nome: {$_POST['nome']}";
+echo '<hr/>';
+echo "Senha: {$_POST['senha']}";
+echo '<hr/>';
+echo "Email: {$_POST['email']}";
+echo '<hr/>';
+echo "Concordo:" . isset($_POST['concordo']) && $_POST['concordo'] != '' ? 'sim' : 'não';
+echo '<hr/>';
+echo "Sexo: {$_POST['sexo']}";
+echo '<hr/>';
+echo "Estado: {$_POST['estado']}";
+echo '<hr/>';
+echo "Descrição: {$_POST['descricao']}";
+echo '<hr/>';
