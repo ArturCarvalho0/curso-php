23bedd7bfcdc0eb081747d5a969f394aefaf2929
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-06-15 16:02:29 -0300

    Upload de multiplos arquivos no PHP

--- uploads/upload.php
+++ uploads/upload.php
@@ -1,11 +1,17 @@
-<?php 
-$photo = $_FILES['foto'];
+<?php
 
+$photos = $_FILES['photos'];
 $path = 'uploads/imgs/';
 
-if (move_uploaded_file($photo['tmp_name'], $path . $photo['name'])) {
-  echo 'Sucesso!';
-}else {
-  echo 'Error!';
+if (!is_dir($path)) {
+  mkdir("uploads");
+  mkdir($path);
 }
 
+for ($i = 0; $i < count($photos['name']); $i++) {
+  $j = $i + 1;
+  if (move_uploaded_file($photos['tmp_name'][$i], $path . $photos['name'][$i]))
+    echo "Sucesso ao mover o arquivo {$j} <br/>";
+  else
+    echo "Falha ao mover o arquivo {$j} <br/>";
+}
