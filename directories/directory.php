3be31ae492299eee2b0caee1729aa25b206cfa30
ArturCarvalho0 <artur.carvalho0@outlook.com>
2023-06-17 11:59:16 -0300

    Manipulação de diretórios no PHP(is_dir,mkdir,rmdir,getcwd,scandir,glob)

---
+++ directories/directory.php
@@ -0,0 +1,11 @@
+<?php 
+echo '<pre/>';
+// var_dump(is_dir('teste'));
+// var_dump(is_dir('test'));
+// var_dump(mkdir('uploads'));
+// var_dump(mkdir('uploads/teste'));
+// var_dump(rmdir('uploads/teste'));
+// var_dump(rmdir('uploads'));
+// var_dump(getcwd());
+// var_dump(scandir('..'));
+// var_dump(glob('../*'));
